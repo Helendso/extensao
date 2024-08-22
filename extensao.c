b64a56141a589332dbf2a75b2f06ace1a7161812
Helendso <86897419+Helendso@users.noreply.github.com>
2024-08-22 00:37:27 +0000

    extensao

---
+++ extensao.c
@@ -0,0 +1,21 @@
+// questão do projeto de extensão
+
+#include <stdio.h>
+#include <string.h>
+
+void q01()
+{
+    char str[50];
+   
+    printf("Digite uma palavra":);
+    scanf( " %50[^\n]", str);
+    
+    int tamanho = strlen(str);
+   
+    printf("O tamanho da string é: %d\n", tamanho);
+   
+}
+int main () {
+    q01();
+    return EXIT_SUCCESS;
+}
