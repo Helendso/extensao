46af4b7036d371ba655853a66134a7331b1691bd
Helendso <86897419+Helendso@users.noreply.github.com>
2024-08-22 01:36:00 +0000

    extensao

--- extensao.c
+++ extensao.c
@@ -1,16 +1,24 @@
 // questão do projeto de extensão
 
-#include <stdio.h>
-#include <string.h>
+#include <stdio.h> 
+#include <string.h> 
 
 
 void q01(){
-    char str[50];
-    printf("Digite uma palavra");
-    scanf( " %50[^\n]", str);
+    //declarando uma variável do tipo string com tamanho de 50 caracteres.
+    char str[50]; 
     
+    //imprimindo texto para o operador executar a função.
+    printf("Digite uma palavra\n");
+
+    //direcionando onde será salvo o comando executado pelo operador.
+    scanf( " %50[^\n]", str);
+
+    //strlen identifica o tamanho da string digitada, aqui estamos declarando uma variável 
+    //e salvando o o tamanho da string nessa variável.
     int tamanho = strlen(str);
-   
+    
+   // imprimindo o valor do tamanho da string que foi digitada pelo operador.
     printf("O tamanho da string é: %d\n", tamanho);
    
 }
