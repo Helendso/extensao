0c8875e350a60daa9d54ccd6dff07e838f38feca
Helendso <86897419+Helendso@users.noreply.github.com>
2024-08-25 19:00:01 +0000

    projeto

--- extensao.c
+++ extensao.c
@@ -6,7 +6,7 @@
 
 void q01(){
   
-    char str[50], str1[50], copia[50], concatenar[50];
+    char str[50], str1[50], copia[50];
     
     printf("Digite uma palavra\n");
     scanf( " %50[^\n]", str);
@@ -14,19 +14,20 @@ void q01(){
     printf("Digite uma palavra\n");
     scanf( " %50[^\n]", str1);
     
+   
     int tamanho = strlen(str);
-    copia[50] = strcpy(copia, str);
-    concatenar[50] = strcat(str,str1);
-    int comparacao = strcmp(str,str1);
-  
-    if (comparacao == 0) {
-       printf("O tamanho da string é: %d\n A String copiada é: %s\n A String Concatenada é: %s\n As Strings comparadas são iguais\n", tamanho, copia, concatenar);
+    strcpy(copia, str1);
+    strcat(str,str1);
+    int cmp=strcmp(str,str1);
+   
+    if (cmp==0) {
+       printf("O tamanho da string é: %d\n A String copiada é: %s\n A String Concatenada é: %s\n As Strings comparadas são iguais\n", tamanho, copia, str);
     }
-    else if (comparacao>0){
-        printf("O tamanho da string é: %d\n A String copiada é: %s\n A String Concatenada é: %s\n A primeira string é maior que a segunda\n", tamanho, copia, concatenar);
+    else if (cmp>0){
+        printf("O tamanho da string é: %d\n A String copiada é: %s\n A String Concatenada é: %s\n A primeira string é maior que a segunda\n", tamanho, copia, str);
     }
     else {
-        printf("O tamanho da string é: %d\n A String copiada é: %s\n A String Concatenada é: %s\n A primeira string é menor que a segunda\n", tamanho, copia, concatenar);
+        printf("O tamanho da string é: %d\n A String copiada é: %s\n A String Concatenada é: %s\n A primeira string é menor que a segunda\n", tamanho, copia, str);
   
     }
 }
